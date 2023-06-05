52a8478563e595f49e2f5514547d657453ea080b
Trinh Minh Nhat <trinhnhathhh@gmail.com>
2023-06-05 22:37:14 +0700

    1.4

---
+++ level1/1.4.js
@@ -0,0 +1,12 @@
+/**
+ * Write a program that takes a list of numbers as input and displays the largest number in the list.
+ * @param {array} []
+ * @return largest number of a
+ */
+
+function theLargestNumber(array){
+    return Math.max(...array);
+}
+
+module.exports = theLargestNumber;
+
